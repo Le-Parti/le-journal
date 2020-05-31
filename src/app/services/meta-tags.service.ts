2489b4ac0c470bb921068657438de9b845ede0db
Adrien Castex <adrien.castex@gmail.com>
2020-05-31 16:52:23 +0200

    Fixed meta tag system for Twitter

--- src/app/services/meta-tags.service.ts
+++ src/app/services/meta-tags.service.ts
@@ -20,9 +20,9 @@ export class MetaTagsService {
       { name: 'description', content: desc },
       { itemprop: 'description', content: desc },
       { itemprop: 'name', content: title },
-      { itemprop: 'image', content: '/assets/img/logo.png' },
+      { itemprop: 'image', content: 'https://le-parti.github.io/le-journal/assets/img/logo.png' },
       
-      { name: 'twitter:image', content: '/assets/img/logo.png' },
+      { name: 'twitter:image', content: 'https://le-parti.github.io/le-journal/assets/img/logo.png' },
       { name: 'twitter:card', content: 'summary_large_image' },
       { name: 'twitter:site', content: '@AdrienCastex' },
       { name: 'twitter:creator', content: '@AdrienCastex' },
@@ -31,7 +31,7 @@ export class MetaTagsService {
       
       { property: 'og:title', content: `Le Journal :: ${title}` },
       { property: 'og:type', content: `website` },
-      { property: 'og:image', content: `/assets/img/logo.png` },
+      { property: 'og:image', content: `https://le-parti.github.io/le-journal/assets/img/logo.png` },
       { property: 'og:url', content: window.location.toString() },
       { property: 'og:description', content: desc },
       { property: 'og:site_name', content: 'Le Journal' }
@@ -45,9 +45,9 @@ export class MetaTagsService {
       { name: 'description', content: article.getDescription(155) },
       { itemprop: 'description', content: article.getDescription(500) },
       { itemprop: 'name', content: article.titlePage },
-      { itemprop: 'image', content: '/assets/img/logo.png' },
+      { itemprop: 'image', content: 'https://le-parti.github.io/le-journal/assets/img/logo.png' },
       
-      { name: 'twitter:image', content: '/assets/img/logo.png' },
+      { name: 'twitter:image', content: 'https://le-parti.github.io/le-journal/assets/img/logo.png' },
       { name: 'twitter:card', content: 'summary_large_image' },
       { name: 'twitter:site', content: '@AdrienCastex' },
       { name: 'twitter:creator', content: '@AdrienCastex' },
@@ -56,7 +56,7 @@ export class MetaTagsService {
       
       { property: 'og:title', content: `Le Journal :: ${article.titlePage}` },
       { property: 'og:type', content: `article` },
-      { property: 'og:image', content: `/assets/img/logo.png` },
+      { property: 'og:image', content: `https://le-parti.github.io/le-journal/assets/img/logo.png` },
       { property: 'og:url', content: article.canonicalUrl },
       { property: 'og:description', content: article.getDescription(500) },
       { property: 'og:site_name', content: 'Le Journal' },
