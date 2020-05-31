5dd94ff0754d159dd3b6539f82e08642fe7530e2
Adrien Castex <adrien.castex@gmail.com>
2020-05-31 08:52:30 +0200

    Changed addTag list to addTags

--- src/app/services/meta-tags.service.ts
+++ src/app/services/meta-tags.service.ts
@@ -16,49 +16,53 @@ export class MetaTagsService {
   public setMetas(title: string, desc: string) {
     this.clear();
 
-    this.metaService.addTag({ name: 'description', content: desc })
-    this.metaService.addTag({ itemprop: 'description', content: desc })
-    this.metaService.addTag({ itemprop: 'name', content: title })
-    this.metaService.addTag({ itemprop: 'image', content: '/assets/img/logo.png' })
-    
-    this.metaService.addTag({ name: 'twitter:image', content: '/assets/img/logo.png' })
-    this.metaService.addTag({ name: 'twitter:card', content: 'summary_large_image' })
-    this.metaService.addTag({ name: 'twitter:site', content: '@AdrienCastex' })
-    this.metaService.addTag({ name: 'twitter:creator', content: '@AdrienCastex' })
-    this.metaService.addTag({ name: 'twitter:title', content: `Le Journal :: ${title}` })
-    this.metaService.addTag({ name: 'twitter:description', content: desc })
-    
-    this.metaService.addTag({ property: 'og:title', content: `Le Journal :: ${title}` })
-    this.metaService.addTag({ property: 'og:type', content: `website` })
-    this.metaService.addTag({ property: 'og:image', content: `/assets/img/logo.png` })
-    this.metaService.addTag({ property: 'og:url', content: window.location.toString() })
-    this.metaService.addTag({ property: 'og:description', content: desc })
-    this.metaService.addTag({ property: 'og:site_name', content: 'Le Journal' })
+    this.metaService.addTags([
+      { name: 'description', content: desc },
+      { itemprop: 'description', content: desc },
+      { itemprop: 'name', content: title },
+      { itemprop: 'image', content: '/assets/img/logo.png' },
+      
+      { name: 'twitter:image', content: '/assets/img/logo.png' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@AdrienCastex' },
+      { name: 'twitter:creator', content: '@AdrienCastex' },
+      { name: 'twitter:title', content: `Le Journal :: ${title}` },
+      { name: 'twitter:description', content: desc },
+      
+      { property: 'og:title', content: `Le Journal :: ${title}` },
+      { property: 'og:type', content: `website` },
+      { property: 'og:image', content: `/assets/img/logo.png` },
+      { property: 'og:url', content: window.location.toString() },
+      { property: 'og:description', content: desc },
+      { property: 'og:site_name', content: 'Le Journal' }
+    ])
   }
 
   public setArticleMetas(article: ArticlesServiceItem) {
     this.clear();
 
-    this.metaService.addTag({ name: 'description', content: article.getDescription(155) })
-    this.metaService.addTag({ itemprop: 'description', content: article.getDescription(500) })
-    this.metaService.addTag({ itemprop: 'name', content: article.titlePage })
-    this.metaService.addTag({ itemprop: 'image', content: '/assets/img/logo.png' })
-    
-    this.metaService.addTag({ name: 'twitter:image', content: '/assets/img/logo.png' })
-    this.metaService.addTag({ name: 'twitter:card', content: 'summary_large_image' })
-    this.metaService.addTag({ name: 'twitter:site', content: '@AdrienCastex' })
-    this.metaService.addTag({ name: 'twitter:creator', content: '@AdrienCastex' })
-    this.metaService.addTag({ name: 'twitter:title', content: `Le Journal :: ${article.titlePage}` })
-    this.metaService.addTag({ name: 'twitter:description', content: article.getDescription(200) })
-    
-    this.metaService.addTag({ property: 'og:title', content: `Le Journal :: ${article.titlePage}` })
-    this.metaService.addTag({ property: 'og:type', content: `article` })
-    this.metaService.addTag({ property: 'og:image', content: `/assets/img/logo.png` })
-    this.metaService.addTag({ property: 'og:url', content: article.canonicalUrl })
-    this.metaService.addTag({ property: 'og:description', content: article.getDescription(500) })
-    this.metaService.addTag({ property: 'og:site_name', content: 'Le Journal' })
+    this.metaService.addTags([
+      { name: 'description', content: article.getDescription(155) },
+      { itemprop: 'description', content: article.getDescription(500) },
+      { itemprop: 'name', content: article.titlePage },
+      { itemprop: 'image', content: '/assets/img/logo.png' },
+      
+      { name: 'twitter:image', content: '/assets/img/logo.png' },
+      { name: 'twitter:card', content: 'summary_large_image' },
+      { name: 'twitter:site', content: '@AdrienCastex' },
+      { name: 'twitter:creator', content: '@AdrienCastex' },
+      { name: 'twitter:title', content: `Le Journal :: ${article.titlePage}` },
+      { name: 'twitter:description', content: article.getDescription(200) },
+      
+      { property: 'og:title', content: `Le Journal :: ${article.titlePage}` },
+      { property: 'og:type', content: `article` },
+      { property: 'og:image', content: `/assets/img/logo.png` },
+      { property: 'og:url', content: article.canonicalUrl },
+      { property: 'og:description', content: article.getDescription(500) },
+      { property: 'og:site_name', content: 'Le Journal' },
 
-    this.metaService.addTag({ property: 'article:published_time', content: new Date(article.dateNum).toISOString() })
-    this.metaService.addTag({ property: 'article:modified_time', content: new Date(article.dateNum).toISOString() })
+      { property: 'article:published_time', content: new Date(article.dateNum).toISOString() },
+      { property: 'article:modified_time', content: new Date(article.dateNum).toISOString() }
+    ])
   }
 }
